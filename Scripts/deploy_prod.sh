8802d7707e2854c1174b9dd54e3529f3f34d07cc
[umaidila] <[frenirus125@gmail.com]>
2022-01-28 20:22:48 +1000

    prod deploy

--- Scripts/deploy_prod.sh
+++ Scripts/deploy_prod.sh
@@ -1,3 +1,3 @@
 git commit -a -m "prod deploy"
 git push
-ssh root@91.203.192.213 'sh /var/www/html/project_prod/Scripts/build.sh'
+ssh root@91.203.192.213 'cd /var/www/html/project_prod/Scripts && bash build.sh'
